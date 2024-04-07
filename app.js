3aadd839aaa26def931fce825fa6b6537c7ef074
Shubham Pal <115977664+beincharacter@users.noreply.github.com>
2024-04-07 19:05:33 +0530

    Added chromecast files

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+const express = require("express");
+const { setupRSocketClient } = require("./rsocketClient");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+setupRSocketClient();
+
+app.get("/", async (req, res) => {
+  try {
+
+    // Send response
+    res.send("Connected to RSocket client");
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
